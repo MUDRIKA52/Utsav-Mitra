4aa8623ee39086d5393ec3f5a3621955b7d2630f
Mudrika <mudrikatiwari45@gmail.com>
2025-04-12 12:46:05 +0530

    modified footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,11 +23,11 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Follow us on:</h3>
           <div className="social-icons">
-            <a href="#">Facebook</a>
-            <a href="#">Twitter</a>
-            <a href="#">Pinterest</a>
-            <a href="#">Instagram</a>
-            <a href="#">YouTube</a>
+            <a href="javascript:void(0)">Facebook</a>
+            <a href="javascript:void(0)">Twitter</a>
+            <a href="javascript:void(0)">Pinterest</a>
+            <a href="javascript:void(0)">Instagram</a>
+            <a href="javascript:void(0)">YouTube</a>
           </div>
 
           <h3>Get The WedMeGood App</h3>
@@ -52,57 +52,57 @@ const Footer = () => {
       <div className="footer-links">
         <div>
           <h4>Start Planning</h4>
-          <a href="#">Search By Vendor</a>
-          <a href="#">Search By City</a>
-          <a href="#">Download Our App</a>
-          <a href="#">Top Rated Vendors</a>
-          <a href="#">Destination Wedding</a>
+          <a href="javascript:void(0)">Search By Vendor</a>
+          <a href="javascript:void(0)">Search By City</a>
+          <a href="javascript:void(0)">Download Our App</a>
+          <a href="javascript:void(0)">Top Rated Vendors</a>
+          <a href="javascript:void(0)">Destination Wedding</a>
         </div>
 
         <div>
           <h4>Wedding Ideas</h4>
-          <a href="#">Wedding Blog</a>
-          <a href="#">Wedding Inspiration Gallery</a>
-          <a href="#">Real Wedding</a>
-          <a href="#">Submit Wedding</a>
+          <a href="javascript:void(0)">Wedding Blog</a>
+          <a href="javascript:void(0)">Wedding Inspiration Gallery</a>
+          <a href="javascript:void(0)">Real Wedding</a>
+          <a href="javascript:void(0)">Submit Wedding</a>
         </div>
 
         <div>
           <h4>Photo Gallery</h4>
-          <a href="#">Bridal Wear</a>
-          <a href="#">Wedding Jewellery</a>
-          <a href="#">Bridal Makeup & Hair</a>
-          <a href="#">Wedding Decor</a>
-          <a href="#">Wedding Photography</a>
-          <a href="#">Groom Wear</a>
-          <a href="#">Invitations & Favors</a>
-          <a href="#">Wedding Accessories</a>
-          <a href="#">Mehendi Designs</a>
+          <a href="javascript:void(0)">Bridal Wear</a>
+          <a href="javascript:void(0)">Wedding Jewellery</a>
+          <a href="javascript:void(0)">Bridal Makeup & Hair</a>
+          <a href="javascript:void(0)">Wedding Decor</a>
+          <a href="javascript:void(0)">Wedding Photography</a>
+          <a href="javascript:void(0)">Groom Wear</a>
+          <a href="javascript:void(0)">Invitations & Favors</a>
+          <a href="javascript:void(0)">Wedding Accessories</a>
+          <a href="javascript:void(0)">Mehendi Designs</a>
         </div>
 
         <div>
           <h4>Home</h4>
-          <a href="#">About WedMeGood</a>
-          <a href="#">Careers</a>
-          <a href="#">Contact Us</a>
-          <a href="#">Site Map</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Cancellation Policy</a>
+          <a href="javascript:void(0)">About WedMeGood</a>
+          <a href="javascript:void(0)">Careers</a>
+          <a href="javascript:void(0)">Contact Us</a>
+          <a href="javascript:void(0)">Site Map</a>
+          <a href="javascript:void(0)">Terms & Conditions</a>
+          <a href="javascript:void(0)">Privacy Policy</a>
+          <a href="javascript:void(0)">Cancellation Policy</a>
         </div>
 
         <div>
           <h4>Wedding Invitation Maker</h4>
-          <a href="#">Wedding Card Designs</a>
-          <a href="#">Save the Date Templates</a>
-          <a href="#">Invitation Video Templates</a>
+          <a href="javascript:void(0)">Wedding Card Designs</a>
+          <a href="javascript:void(0)">Save the Date Templates</a>
+          <a href="javascript:void(0)">Invitation Video Templates</a>
         </div>
       </div>
 
       {/* Copyright */}
       <div className="footer-bottom">
         <p>© 2025 WedMeGood</p>
-        <p><a href="#">Terms & Conditions</a> | <a href="#">Privacy Policy</a></p>
+        <p><a href="javascript:void(0)">Terms & Conditions</a> | <a href="javascript:void(0)">Privacy Policy</a></p>
       </div>
     </footer>
   );
